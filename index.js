ddc8feed323c2dc223a64ff1dcd3d74f59fffd75
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-04 14:09:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+function map(arr, fn) {
+  return arr.map(item => fn(item))
+}
+
+function reduce(arr, fn, int=0) {
+  if (!int) {
+    return arr.reduce((accum, item) => fn(accum, item))
+  } else {
+    return arr.reduce((accum, item, int) => fn(accum, item), int)
+  }
+}
